bb44e5942b7518a19a30209ecfb4a726ab07e55d
SURAMERICANA\carlvarn <1037636590>
2017-07-23 10:31:26 -0500

    Problem description in readme Project structure .gitignote Dependencies

---
+++ src/main/java/com/codewars/MorseCodeDecoder.java
@@ -0,0 +1,14 @@
+package com.codewars;
+
+/**
+ * Created by carlvarn on 23/07/2017.
+ */
+public class MorseCodeDecoder {
+    public static String decodeBits(String bits) {
+        return "";
+    }
+
+    public static String decodeMorse(String morseCode) {
+        return MorseCode.get(morseCode);
+    }
+}
